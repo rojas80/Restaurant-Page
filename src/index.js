5411659df98c16471468231fe15c13eca9c1f2e1
hugo rojas <rojashugo80@gmail.com>
2024-01-11 17:47:39 -0800

    Webpack project has been setup

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import '../src/style.css';
+
+console.log("We are ready to start");
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer:{
+        static:{
+            directory: path.resolve(__dirname,'dist'),
+        },
+        port:3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    }
+    ,
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader'],
+            },
+        ],
+    },
+
+};
